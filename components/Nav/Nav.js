5dbac324be6c6a6c65c5f0bebda543ba8facf9bf
yassineelhoubi <elhoubiyassine@gmail.com>
2022-02-10 14:21:24 +0100

    custom app

---
+++ components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Navbar</span>
+  </div>
+</nav>
+  );
+};
+
+export default Nav;
