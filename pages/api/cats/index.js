917661f83b2cadf1ae0dfaa041d734fb8b01f2f5
yassineelhoubi <elhoubiyassine@gmail.com>
2022-02-10 20:05:32 +0100

    API Endpoints

--- pages/api/cats/index.js
+++ pages/api/cats/index.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { cats } from "../../../data";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(cats)
 }
+// localhost:3000/api/hello
