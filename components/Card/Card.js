0bb7d6a2bda1a8b0d01d8005d3c7cdf34446cf9f
yassineelhoubi <elhoubiyassine@gmail.com>
2022-02-10 21:10:23 +0100

    fetching data

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from "./Card.module.css"
 const Card = ({ name, phone, email, image, id }) => {
     return (
-        <div className={styles.card}>
-            <div className={styles.card - header}>
-                <img src={image.url} alt={image.alt} className={styles.card - img} />
+        <div className={styles.card} key={id}>
+            <div className={styles["card-header"]}>
+                <img src={image.url} alt={image.alt} className={styles["card-img"]} />
             </div>
             <div >
                 <h3>{name}</h3>
--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from '../../components/Nav/Nav'
-
+import Card from '../../components/Card/Card'
 const index = () => {
+
+  const [cats, setCats] = useState([])
+  const fetchCats = async () => {
+    const response = await fetch('/api/cats')
+    const data = await response.json();
+    console.log(data)
+    setCats(data)
+  }
+  useEffect(() => {
+    fetchCats()
+  }, [])
   return (
-    <div>
+    <>
       <Nav />
-    </div>
+      <div className="container mt-5">
+        <div className="d-flex flex-wrap justify-content-between">
+
+        {cats.map(cat => (
+          <Card
+            name={cat.name}
+            id={cat.id}
+            phone={cat.phone}
+            image={cat.image}
+            email={cat.email}
+          />
+        ))}
+        </div>
+      </div>
+    </>
   )
 }
 
